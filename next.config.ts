a28a98cd668584d2a6a5bc02a16bfc5a2e6abcdd
faisa <faisalprofessional1@gmail.com>
2024-12-23 18:55:50 +0530

    production url clean

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   env: {
     api_local: 'http://localhost:4000/api',
-    api_prod: "https://kanjkiro-backend.vercel.app/"
+    api_prod: "https://kanjkiro-backend.vercel.app/api"
   },
 };
 
